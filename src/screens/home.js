f46519521998d9f5f1e531ba9024d3d94e8d9eb0
Urip Subekti <urip.subekti13@gmail.com>
2021-04-09 07:52:26 +0700

    feat init: Add React Navigation

---
+++ src/screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+import { ScaledText } from 'urip-rn-kit'
+
+const HomeScreen = (props) => {
+  return (
+    <SafeAreaView>
+      <ScaledText>Home</ScaledText>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen;
